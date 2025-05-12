38d41fb30dc799a4e2652aadce92b2804fa3dfae
Glitch-txs <glitch.txs@gmail.com>
2025-05-12 16:35:29 -0300

    fix type inference

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ type K<T> = Extract<keyof T, string>
 type V<T> = T[Extract<keyof T, string>]
 
 export class Store<TStore extends Object> extends Map<K<TStore>, V<TStore>> {
-  callbacks = new Map<K<TStore>, Set<(prev: V<TStore>)=> unknown>>()
-  onCallbacks(key: K<TStore>, value: V<TStore>){
+  private callbacks = new Map<K<TStore>, Set<(prev: V<TStore>)=> unknown>>()
+  private onCallbacks(key: K<TStore>, value: V<TStore>){
     this.callbacks.get(key)?.forEach(callback => callback(value))
   }
 
@@ -17,7 +17,11 @@ export class Store<TStore extends Object> extends Map<K<TStore>, V<TStore>> {
     super(Object.entries(initialValues) as [K<TStore>, V<TStore>][])
   }
 
-  set(key: K<TStore>, value: V<TStore>) {
+  get<Key extends K<TStore>>(key: Key): TStore[Key] | undefined {
+    return super.get(key) as TStore[Key];
+  }
+
+  set<Key extends K<TStore>>(key: Key, value: TStore[Key]) {
     const prev = this.get(key)
     if(prev === value) return this
     
@@ -26,26 +30,26 @@ export class Store<TStore extends Object> extends Map<K<TStore>, V<TStore>> {
     return this
   }
 
-  update(key: K<TStore>, value: (prev: V<TStore>)=> V<TStore>) {
+  update<Key extends K<TStore>>(key: Key, value: (prev: TStore[Key]) => TStore[Key]) {
     if(!this.has(key)) throw Error(`Calling update: "${key}" was not initialized.`)
-    const prev = this.get(key) as V<TStore>
+    const prev = this.get(key) as TStore[Key]
     const _newVal = value(prev)
-    if(prev === _newVal) return
+    if(prev === _newVal) return this
 
     super.set(key, _newVal)
     this.onCallbacks(key, _newVal)
     return this
   }
 
-  subscribe(key: K<TStore>, callback: (prev: V<TStore>)=> unknown) {
+  subscribe<Key extends K<TStore>>(key: Key, callback: (prev: TStore[Key]) => unknown) {
     if (typeof callback !== 'function') {
       throw new Error('Callback must be a function')
     }
     const callbacks = this.callbacks.get(key) ?? new Set()
-    callbacks.add(callback)
+    callbacks.add(callback as (prev: V<TStore>) => unknown)
     this.callbacks.set(key, callbacks)
     return ()=>{
-      this.callbacks.get(key)?.delete(callback)
+      this.callbacks.get(key)?.delete(callback as (prev: V<TStore>) => unknown)
     }
   }
 
